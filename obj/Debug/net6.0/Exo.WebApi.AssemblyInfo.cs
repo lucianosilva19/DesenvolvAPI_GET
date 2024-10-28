80bbb3b2b0e0d9d44041d17738ecae757549313c
Luciano da Silva <luciano.silva19@aluno.senai.br>
2024-10-27 23:25:34 -0300

    DevAPI_CRUDfinal

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec146f19e5668e0b3c5e5328d4fa8ea1b73258c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e94eff87e862624dc79e5c57d4204b5827f348")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
